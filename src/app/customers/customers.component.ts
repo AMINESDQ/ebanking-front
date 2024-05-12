ffae55b6053a407a13e8972547f661d0a08b2950
AMINESDQ <aminesadiq05@gmail.com>
2024-05-12 21:32:00 +0200

    first commit

---
+++ src/app/customers/customers.component.ts
@@ -0,0 +1,61 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { error } from 'console';
+import { CustomersService } from '../services/customers.service';
+import { Observable, catchError, map, throwError } from 'rxjs';
+import { Customer } from '../model/customer.model';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-customers',
+  templateUrl: './customers.component.html',
+  styleUrl: './customers.component.css'
+})
+export class CustomersComponent implements OnInit {
+  customers! : Observable<Array<Customer>>;
+  errorMessage!: string;
+  searchFormGroup : FormGroup | undefined;
+  constructor(private cs:CustomersService ,private fb : FormBuilder, private router : Router) { }
+  ngOnInit(): void {
+    this.searchFormGroup=this.fb.group({
+      keyword : this.fb.control("")
+    });
+    this.handleSearchCustomers();
+  }
+  handleSearchCustomers() {
+    let kw=this.searchFormGroup?.value.keyword;
+    this.customers=this.cs.searchCustomers(kw).pipe(
+      catchError(err => {
+        this.errorMessage=err.message;
+        return throwError(err);
+      })
+    );
+  }
+
+  handleDeleteCustomer(c: Customer) {
+    let conf = confirm("Are you sure?");
+    if(!conf) return;
+    this.cs.deleteCustomer(c.id).subscribe({
+      next : (resp) => {
+        this.customers=this.customers.pipe(
+          map(data=>{
+            let index=data.indexOf(c);
+            data.slice(index,1)
+            return data;
+          })
+        );
+      },
+      error : err => {
+        console.log(err);
+      }
+    })
+  }
+
+  handleCustomerAccounts(customer: Customer) {
+    this.router.navigateByUrl("/customer-accounts/"+customer.id,{state :customer});
+  }
+
+  
+
+}
